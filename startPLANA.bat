eeef71fe116e2450456d0f971449d5a3205978a0
coffin299 <coffin299@ymail.ne.jp>
2025-10-07 12:42:56 +0900

    venvを作成するように変更

--- startPLANA.bat
+++ startPLANA.bat
@@ -1,4 +1,23 @@
 ECHO PLANA STARTER
+
+set VENV_DIR=venv
+
+if not exist "%VENV_DIR%" (
+    ECHO Creating virtual environment in '%VENV_DIR%' folder...
+    python -m venv %VENV_DIR%
+    if %errorlevel% neq 0 (
+        ECHO Failed to create virtual environment.
+        pause
+        exit /b
+    )
+)
+
+ECHO Activating virtual environment...
+call "%VENV_DIR%\Scripts\activate.bat"
+
+ECHO Installing/Updating required packages...
 python -m pip install -U -r requirements.txt
+
+ECHO Starting the application...
 python main.py
 pause
