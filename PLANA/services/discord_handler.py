1d16dc1a26f1660e377aff823b9801f01fbbd2fc
coffin299 <coffin299@ymail.ne.jp>
2025-10-07 21:14:48 +0900

    change:[main.py,discord_handler.py]ログの色を変更

--- PLANA/services/discord_handler.py
+++ PLANA/services/discord_handler.py
@@ -18,12 +18,12 @@ class DiscordLogFormatter(logging.Formatter):
     RESET = "\u001b[0m"
     RED = "\u001b[31m"
     YELLOW = "\u001b[33m"
-    GREEN = "\u001b[32m"
+    BLUE = "\u001b[34m"
     WHITE = "\u001b[37m"
 
     COLOR_MAP = {
         logging.DEBUG: WHITE,
-        logging.INFO: GREEN,
+        logging.INFO: BLUE,
         logging.WARNING: YELLOW,
         logging.ERROR: RED,
         logging.CRITICAL: RED,
