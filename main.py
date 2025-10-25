138ef3e41e5bef245483e0c2b4442a956c3f9008
coffin299 <coffin299@ymail.ne.jp>
2025-10-25 13:45:27 +0900

    change:[main.py] 特権インテント申請が受理されたので intents.message_content = True に変更

--- main.py
+++ main.py
@@ -354,7 +354,7 @@ if __name__ == "__main__":
     intents.guild_messages = True
     intents.dm_messages = True
     intents.voice_states = True
-    intents.message_content = False #特権インテントの申請が受理されたらTrueに変更
+    intents.message_content = True #特権インテントの申請が受理されたらTrueに変更
     intents.members = False
     intents.presences = False
     allowed_mentions = discord.AllowedMentions(everyone=False, users=True, roles=False, replied_user=True)
